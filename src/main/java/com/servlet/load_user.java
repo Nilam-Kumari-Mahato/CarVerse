37556f4f8892052382b684ee4ac41665ad630662
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2026-09-05 02:01:48 +0530

    View profile for user added

---
+++ src/main/java/com/servlet/load_user.java
@@ -0,0 +1,78 @@
+package com.servlet;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class load_user {
+    private String u_id;
+    private String u_name;
+    private String u_email;
+    private String u_pass;
+    private String u_cont;
+    private String u_add;
+    private String u_createdat;
+
+    public String getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(String u_id) {
+        this.u_id = u_id;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+
+    public String getU_email() {
+        return u_email;
+    }
+
+    public void setU_email(String u_email) {
+        this.u_email = u_email;
+    }
+
+    public String getU_pass() {
+        return u_pass;
+    }
+
+    public void setU_pass(String u_pass) {
+        this.u_pass = u_pass;
+    }
+
+    public String getU_cont() {
+        return u_cont;
+    }
+
+    public void setU_cont(String u_cont) {
+        this.u_cont = u_cont;
+    }
+
+    public String getU_add() {
+        return u_add;
+    }
+
+    public void setU_add(String u_add) {
+        this.u_add = u_add;
+    }
+
+    public String getU_createdat() {
+        return u_createdat;
+    }
+
+    public void setU_createdat(String u_createdat) {
+        this.u_createdat = u_createdat;
+    }
+    
+    
+}
